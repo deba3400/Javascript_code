460cd02ab5b7d6382ce5a45f5aa4f629fa7dd02b
deba3400 <debashisadhikari567@gmail.com>
2024-05-22 18:25:16 +0530

    code added

---
+++ MathFunction/mathFunction1.js
@@ -0,0 +1,30 @@
+//abs
+//abs(positive_value)
+//abs(negative_value)
+
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(9.5));
+// console.log(Math.ceil(7.5));
+// console.log(Math.max(8,4,9));
+// console.log(Math.sin(45));
+
+
+
+
+// print numbers between the range
+function randomInrange(x,y){
+
+    return console.log(Math.random()*(y-x+1)+x);
+}
+
+randomInrange(4,5);
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
--- Scope/TDZ.js
+++ Scope/TDZ.js
@@ -3,9 +3,9 @@ console.log("hello ");
 
 function fun(){
     console.log("i am in fun()");
-    console.log(x);
-    console.log(;
-    
-let x=10;
+    console.log(x);// X is in TDZ bcz it is declare using let and above the declaration line it has temporal dead zone   
+    let x=10;
 }
+fun();
+
 
